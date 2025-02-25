5f2267ddb3aa765586d5910bdb6bafa91d6c1a8c
Anna <denisova.2010@yandex.ru>
2025-02-25 14:42:39 +0300

    Delete 33f.py

--- 33f.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "http://mfd.ru/currency/?currency=USD"
-headers = {"User-Agent": "Mozilla/5.0"}
-
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, "html.parser")
